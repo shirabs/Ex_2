1181770b10004f27dd12e17b9297498ecd434914
shirabs <shira159159@gmail.com>
2019-12-22 13:18:43 +0200

    continue DGraph2

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 public class DGraph implements graph{
 
-	private HashMap<Integer,NodeData> DataMap;
-	private HashMap<Integer, HashMap<Integer, EdgeData>> EdgeMap;
+	private HashMap<Integer,node_data> DataMap;
+	private HashMap<Integer, HashMap<Integer, edge_data>> EdgeMap;
 	private int countEdge=0;
+	private int mc=0;
 
 	public DGraph() {
-		this.DataMap=new HashMap<Integer, NodeData>();
-		this.EdgeMap=new HashMap<Integer, HashMap<Integer, EdgeData>>();
+		this.DataMap=new HashMap<Integer, node_data>();
+		this.EdgeMap=new HashMap<Integer, HashMap<Integer, edge_data>>();
 		this.countEdge=0;
+		this.mc=0;
 	}
 
 	@Override
@@ -33,7 +37,6 @@ public class DGraph implements graph{
 			}
 		}
 		return null;
-
 	}
 
 
@@ -41,6 +44,7 @@ public class DGraph implements graph{
 	public void addNode(node_data n) {
 		int key=n.getKey();
 		DataMap.put(key, (NodeData) n);
+		mc++;
 	}
 
 
@@ -53,49 +57,55 @@ public class DGraph implements graph{
 		else {
 			EdgeData x=new EdgeData(src, dest, w);
 			if(this.EdgeMap.get(src)==null) {
-				this.EdgeMap.put(src,new HashMap<Integer,EdgeData>());
+				this.EdgeMap.put(src,new HashMap<Integer,edge_data>());
 				this.EdgeMap.get(src).put(src, x);
-countEdge++;
+				countEdge++;
 			}
 			else {
 				this.EdgeMap.get(src).put(src, x);
 				countEdge++;
 			}
-
-
-
-
+			mc++;
 		}
 	}
 
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.DataMap.values();
 	}
 
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<Integer, edge_data> t= this.EdgeMap.remove(node_id);
+		this.EdgeMap.put(node_id, t);
+		return t.values();
 	}
 
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		node_data t= this.DataMap.remove(key);
+		Collection<edge_data> rem=this.EdgeMap.remove(key).values();
+		countEdge-=rem.size();
+		for(int i=0;i<EdgeMap.size();i++) {
+			removeEdge(i, key);
+			countEdge--;
+		}
+		mc++;
+		return t;
 	}
 
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		mc++;
+		countEdge--;
+		return this.EdgeMap.get(src).remove(dest);
 	}
 
 	@Override
 	public int nodeSize() {
 		// TODO Auto-generated method stub
-		return 0;
+		return DataMap.size() ;
 	}
 
 	@Override
@@ -107,7 +117,7 @@ countEdge++;
 	@Override
 	public int getMC() {
 		// TODO Auto-generated method stub
-		return 0;
+		return mc;
 	}
 	public String toString() {
 		return  "the countedge ="+countEdge;
